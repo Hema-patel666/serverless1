a18bc15ab4e3a1bc195397b6fd1e35130df3be4d
varsha <var6997@gmail.com>
2022-02-11 13:27:58 +0530

    first commit

---
+++ Controller/product.js
@@ -0,0 +1,84 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const uuid = require('uuid').v4;
+
+const dbConnection = require('../config/dbconfig');
+const ProductService = require('../Services/product');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//  base url to test our API
+app.get('/index', async (req, res) => {
+   await res.send("Welcome to the Product API for LogRocket Blog serverless Example!!")
+})
+
+
+//  function for creating a new product
+app.post('/in',async(req,res)=>{
+    console.log("Hello")
+    await res.send("hello")
+})
+
+app.post('/add', async (req, res) => {
+  try {
+      console.log("postdata...")
+      await dbConnection();
+   const data  = req.body;
+   const {name, type, description, cost} = data;
+ if(!data) 
+ {
+     return "Please pass all required fields!"
+  }
+   const dataToSave = {name,type,description,cost,productId:uuid()};
+   let createProduct =  await ProductService.createProduct(dataToSave);
+   if (createProduct) {
+     return res.status(200).send(
+       createProduct
+    )
+   }
+  }
+   catch (error) {
+   
+    console.log(error, "error!!");
+  }
+})
+
+//  function for getting all products
+app.get('/lists', async (req, res) => {
+try {
+    await dbConnection();
+    const allProducts = await ProductService.getAllProduct();
+    if (allProducts) {
+      return res.status(200).send({
+        data: allProducts,
+        msg:"hello"
+      })
+    }
+  } catch (error) {
+     //  handle errors here
+     console.log(error, "error!!");
+  }
+})
+
+
+//  function for getting a  product by Id
+app.get('/:productId', async (req, res) => {
+  try {
+    await dbConnection();
+    const id = req.params.productId;
+    const getProduct = await ProductService.getProductById(id);
+    if(getProduct) {
+      return res.status(200).send({
+        data: getProduct
+      })
+    }
+  } catch (error) {
+     //  handle errors here
+     console.log(error, "error!!");
+  }
+});
+
+module.exports.handler = serverless(app);
---
+++ config/dbconfig.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+mongoose.Promise = global.Promise;
+
+const connectToDatabase = async () => {
+  let isConnected;
+  if (isConnected) {
+    console.log('using existing database connection');
+    return Promise.resolve();
+  }
+
+  console.log('using new database connection');
+  const database = await mongoose.connect(process.env.DB,
+     {
+         useNewUrlParser: true
+        });
+  isConnected = database.connections[0].readyState;
+  // return isConnected;
+};
+
+module.exports = connectToDatabase;
